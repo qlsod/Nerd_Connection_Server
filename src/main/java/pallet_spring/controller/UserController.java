c430ab67757e68e1844a9b6637c1843d51ec59b0
qlsod <qlsod9570@naver.com>
2023-10-17 19:42:29 +0900

    signUp 코드 수정(다중 if문 삭제)

--- src/main/java/pallet_spring/controller/UserController.java
+++ src/main/java/pallet_spring/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -29,36 +29,38 @@ public class UserController {
     private UserService userService;
 
     // Data 조회 시 Get
-    @GetMapping("/no/{no}")
+    @GetMapping("/{no}")
     public User getUser(@PathVariable("no") int no) {
         return userMapper.getUserProfile(no);
     }
 
     @PostMapping("/login")
+
+    // 인증 토큰 넘겨야 함
+
+
+
     public String userLogin(@RequestBody @Valid LoginDTO loginDTO, BindingResult bindingResult, HttpSession session) {
         if (bindingResult.hasErrors()) {
             log.error("유효성 검사 오류 발생");
-            Map<String, String> errors = new HashMap<>();
-
-            for (FieldError error : bindingResult.getFieldErrors()) {
-                errors.put(error.getField(), error.getDefaultMessage());
-            }
+//            Map<String, String> errors = new HashMap<>();
+//
+//            for (FieldError error : bindingResult.getFieldErrors()) {
+//                errors.put(error.getField(), error.getDefaultMessage());
+//            }
             throw new RuntimeException("NotBlank 조건 충족 못함");
-        }else {
-
+        } else {
             boolean loginResult = userService.login(loginDTO);
-            log.info(String.valueOf(loginDTO));
             if (loginResult) {
                 session.setAttribute("loginId", loginDTO.getId());
-                return "성공";
+                return "로그인 성공";
             } else {
                 throw new RuntimeException("가입된 계정이 없습니다.");
             }
         }
-
     }
 
-    @GetMapping("/all")
+    @GetMapping("")
     public List<User> getUserList() {
         return userService.findUserList();
     }
@@ -67,7 +69,7 @@ public class UserController {
     // Data 생성 시 POST
     // 전처리 위한 @Vaild 이용
     @PostMapping("/signUp")
-    public User postUsers(@RequestBody @Valid User user, BindingResult bindingResult) {
+    public String postUsers(@RequestBody @Valid User user, BindingResult bindingResult) {
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();  // 암호 강도 Default 10
         String result = encoder.encode("pw");
@@ -75,39 +77,19 @@ public class UserController {
         // 전처리(유효성) 검사 불통과 할 시
         if (bindingResult.hasErrors()) {
             log.error("hasError 발생");
-            Map<String, String> errors = new HashMap<>();
 
-            for (FieldError error : bindingResult.getFieldErrors()) {
-                errors.put(error.getField(), error.getDefaultMessage());
-            }
+//            // 에러가 난 이유 표시 하고 싶을 시
+//            Map<String, String> errors = new HashMap<>();
+//
+//            for (FieldError error : bindingResult.getFieldErrors()) {
+//                errors.put(error.getField(), error.getDefaultMessage());
+//            }
             throw new RuntimeException("NotBlank 충족 못함");
         } else {
             userService.signUp(user);
-            return user;
+            return "회원가입 성공";
         }
-
     }
 
-//    // Data 수정 시 PUT(잘 안씀)
-//    // Data 수정 시 Patch
-//    @PutMapping("/user/{id}")
-//    public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("pw") String pw) {
-//        userMapper.updateUserProfile(id, name, pw);
-//        // DB 연결 안된 경우
-//        // UserProfile userProfile = userMap.get(id);
-//        // userProfile.setName(name);
-//        // userProfile.setPw(pw);
-//    }
-//
-//    // Data 삭제 시 Del
-//    @DeleteMapping("/user/{id}")
-//    public void deleteUserProfile(@PathVariable("id") String id) {
-//        userMapper.deleteProfile(id);
-//        // DB 연결 안된 경우
-//        // userMap.remove(id);
-//
-//
-//        // create, update, delete
-//
-//    }
+
 }
