068381e22e8ff004847a27cd7408d2793694b1fb
qlsod <qlsod9570@naver.com>
2023-10-23 17:09:17 +0900

    security 기본 세팅 및 폴더 구조 조정

--- src/main/java/pallet_spring/securityConfig/AuthenticationConfig.java
+++ src/main/java/pallet_spring/securityConfig/AuthenticationConfig.java
@@ -1,3 +1,5 @@
+package pallet_spring.securityConfig;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,14 +12,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AuthenticationConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
         httpSecurity
                 .httpBasic().disable() // 기본 HTTP 인증을 비활성화
-                .csrf().disable() // CSRF 보호를 비활성화
-                .cors().and() // CORS 보호를 활성화 (필요하다면 `.configurationSource()`를 사용하여 추가 구성 가능)
+                .cors().disable() // CORS 보호를 활성화 (필요하다면 `.configurationSource()`를 사용하여 추가 구성 가능)
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/users/signup").permitAll() // /users/signup 경로에 대한 POST 요청은 모두 허용
-                .and();
+                .antMatchers(HttpMethod.POST,"/users/signup").permitAll()// /users/signup 경로에 대한 POST 요청은 모두 허용
+                .antMatchers(HttpMethod.POST, "/users/login").permitAll()// /users/login 경로에 대한 POST 요청은 모두 허용
+//                .antMatchers(HttpMethod.GET, "/users").permitAll()     // get 요청 test 확인 시 삭제
+                .anyRequest().authenticated();
 
         return httpSecurity.build();
     }
+
+
+
+
 }
