e27c65cd819a3fab4ac760e8836e03b254c98077
qlsod <qlsod9570@naver.com>
2023-11-10 14:35:02 +0900

    valid 검사에 따른 예외처리 생성

--- src/main/java/pallet_spring/handler/ExceptionResponseHandler.java
+++ src/main/java/pallet_spring/handler/ExceptionResponseHandler.java
@@ -2,10 +2,17 @@ package pallet_spring.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Slf4j
@@ -16,10 +23,30 @@ public class ExceptionResponseHandler {
     // 예외 처리
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(RuntimeException.class)
-    public String validationException(RuntimeException e) {
-        log.info("RuntimeException처리");
+    public String customRuntimeException(RuntimeException e) {
         return e.getMessage();
     }
+
+
+    // valid 불충
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> validationException(MethodArgumentNotValidException e, HttpServletResponse response) {
+
+        response.setStatus(response.SC_BAD_REQUEST);
+        BindingResult bindingResult = e.getBindingResult();
+
+        final Map<String, Object> body = new HashMap<>();
+
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            body.put("status", response.SC_BAD_REQUEST);
+            body.put("field", error.getField());
+            body.put("message", error.getDefaultMessage());
+        }
+
+        return body;
+    }
+
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
