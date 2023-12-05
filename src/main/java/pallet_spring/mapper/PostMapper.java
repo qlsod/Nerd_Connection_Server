e881e0fb0370a72b7c39890ef9670dd1de8d74d0
qlsod <qlsod9570@naver.com>
2023-12-05 18:00:50 +0900

    myImages API 에 따른 image_url, update_date, post_no 반환 Mapper 작성

--- src/main/java/pallet_spring/mapper/PostMapper.java
+++ src/main/java/pallet_spring/mapper/PostMapper.java
@@ -2,6 +2,7 @@ package pallet_spring.mapper;
 
 import org.apache.ibatis.annotations.*;
 import pallet_spring.model.Image;
+import pallet_spring.model.MyImage;
 import pallet_spring.model.Post;
 import java.util.List;
 
@@ -42,6 +43,23 @@ public interface PostMapper {
             "ORDER BY update_date DESC " +
             "LIMIT 2")
     List<Image> getNextImage(@Param("no") int no);
+
+    @Results(id = "PostMap2", value = {
+            @Result(property = "post_no", column = "post_no"),
+            @Result(property = "user_no", column = "user_no"),
+            @Result(property = "content", column = "content"),
+            @Result(property = "photo_url", column = "photo_url"),
+            @Result(property = "share_check", column = "share_check"),
+            @Result(property = "create_date", column = "create_date"),
+            @Result(property = "update_date", column = "update_date"),
+            @Result(property = "delete_date", column = "delete_date")
+    })
+    @Select("SELECT photo_url, post_no, posts.update_date FROM users " +
+            "JOIN posts On users.no = posts.user_no " +
+            "WHERE users.no = #{userNo} " +
+            "AND DATE_FORMAT(posts.update_date, '%Y-%m') = #{targetTime} " +
+            "ORDER BY update_date ASC")
+    List<MyImage> getMyImage(@Param("userNo") int userNo, @Param("targetTime") String targetTime);
 
 
 }
