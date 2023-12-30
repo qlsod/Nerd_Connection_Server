bab7c40d1d5d32dfa8e13c5ba1b261f1a353f040
qlsod <qlsod9570@naver.com>
2023-12-30 12:38:26 +0900

    /posts swagger 설명 지정 및 리턴 데이터 값 수정

--- src/main/java/pallet_spring/controller/PostController.java
+++ src/main/java/pallet_spring/controller/PostController.java
@@ -1,9 +1,17 @@
 package pallet_spring.controller;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pallet_spring.mapper.PostMapper;
@@ -11,20 +19,19 @@ import pallet_spring.mapper.UserMapper;
 import pallet_spring.model.Image;
 import pallet_spring.model.MyImage;
 import pallet_spring.model.Post;
+import pallet_spring.model.response.ImageRes;
 import pallet_spring.security.jwt.JwtProvider;
 import pallet_spring.service.PostService;
 import pallet_spring.service.UserService;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
+@Tag(name = "Post", description = "게시글 관련 api")
 @RequestMapping("/posts")
 public class PostController {
-
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -52,7 +59,18 @@ public class PostController {
 
     // 따로 뺄 수도 있음(현재는 post에서만 사용)
     @PostMapping("upload/image")
-    public Map<String, Object> postImageUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+    @Operation(summary = "이미지 URL 반환",
+            description = "이미지 S3 저장 후 URL 반환")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "저장 성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
+    public ResponseEntity<ImageRes> postImageUpload(
+            @Parameter(description = "이미지 form-data 형식으로 담아 키값 이름 file로 설정하여 요청")
+            @RequestParam(name = "file", required = false) MultipartFile file, HttpServletRequest request) {
+
+        // 파일 존재 여부 확인
+        postService.validateFileExists(file);
 
         // 토큰에 저장된 유저 ID 꺼내는 로직
         String userId = jwtProvider.getUserIdLogic(request);
@@ -60,37 +78,68 @@ public class PostController {
         // S3 upload
         String photo_url = postService.uploadS3(file, userId);
 
-        Map<String, Object> url = new HashMap<>();
-        url.put("photo_url", photo_url);
+        ImageRes imageRes = new ImageRes();
+        imageRes.setPhoto_url(photo_url);
 
-        return url;
+        return ResponseEntity.status(HttpStatus.CREATED).body(imageRes);
     }
 
     @PostMapping("upload")
-    public String postUpload(@RequestBody @Valid Post post, HttpServletRequest request) {
+    @Operation(summary = "다이어리 게시글 저장",
+            description = "이미지 URL 받아 게시글과 함께 저장")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "저장 성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
+    public ResponseEntity<Void> postUpload(@RequestBody @Valid Post post, HttpServletRequest request) {
 
         // 토큰에 저장된 유저 ID 꺼내는 로직
         String userId = jwtProvider.getUserIdLogic(request);
 
         postService.postUpload(post, userId);
 
-        return "성공";
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     // 공유 가능 이미지 불러오기
     @GetMapping("image/all")
+    @Operation(summary = "공유 가능 이미지 불러오기 처음 시도하는 api",
+            description = "공유 체크한 이미지 불러오기(페이징 처리로 인한 18개 제한)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
     public List<Image> returnAllImageURL() {
         return postMapper.getAll();
     }
 
     // 마지막 image의 no값 받아 다음 아이템 표시
     @GetMapping("image/{no}")
-    public List<Image> returnNextImageURL(@PathVariable("no") int no) {
+    @Operation(summary = "공유가능 이미지 불러오기 두번째 부터",
+            description = "첫번째 api에서 반환된 post_no 중 가장 마지막 post_no 입력받아 다음 이미지 표시")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
+    public List<Image> returnNextImageURL(
+            @Parameter(description = "마지막 post_no 값", example = "1")
+            @PathVariable("no") int no) {
+
         return postMapper.getNextImage(no);
     }
 
     @GetMapping("myimages/{targetTime}")
-    public List<MyImage> returnMyImageURL(HttpServletRequest request, @PathVariable("targetTime") String targetTime) {
+    @Operation(summary = "달력에 표시할 이미지 불러오기",
+            description = "해당 월의 작성된 이미지 불러오기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
+    @SecurityRequirement(name = "accessToken")
+    public List<MyImage> returnMyImageURL(
+            @Parameter(description = "해당 연도, 월 입력", example = "2023-12")
+            @PathVariable("targetTime") String targetTime,
+            HttpServletRequest request) {
 
         // 토큰에 저장된 유저 ID 꺼내는 로직
         String id = jwtProvider.getUserIdLogic(request);
@@ -104,7 +153,15 @@ public class PostController {
 
     // 해당글 조회
     @GetMapping("{post_no}")
-    public Post getPostDetail(@PathVariable("post_no") int post_no) {
+    @Operation(summary = "달력에 표시된 이미지의 상세내용 불러오기",
+            description = "해당 게시글의 상세내용 표시")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "실패")
+    })
+    public Post getPostDetail(
+            @Parameter(description = "해당 post_no 입력", example = "2023-12")
+            @PathVariable("post_no") int post_no) {
         Post post = postMapper.getPostDetail(post_no);
         if(post == null) {
             throw new RuntimeException("해당 글이 존재하지 않습니다.");
