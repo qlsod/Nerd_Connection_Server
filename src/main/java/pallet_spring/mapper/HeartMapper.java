ecf184af6c630e94062b3709dcae26b3869d2145
qlsod <rlatpals100@naver.com>
2024-06-15 20:25:50 +0900

    좋아요 표시 클릭 Api에 따른 HeartMapper 추가

---
+++ src/main/java/pallet_spring/mapper/HeartMapper.java
@@ -0,0 +1,18 @@
+package pallet_spring.mapper;
+
+import org.apache.ibatis.annotations.*;
+import pallet_spring.model.HeartDto;
+
+@Mapper
+public interface HeartMapper {
+
+    @Select("SELECT COUNT(*) > 0 FROM hearts JOIN users ON hearts.user_no = users.no WHERE users.id = #{user_id} AND post_no = #{post_no}")
+    boolean checkLikeUser(@Param("user_id") String user_id, @Param("post_no") int post_no);
+
+
+    @Insert("INSERT INTO hearts(user_no, post_no)" +
+            "VALUES" +
+            "(#{heart.user_no}, #{heart.post_no})")
+    void insertHeart(@Param("heart") HeartDto heartDto);
+
+}
