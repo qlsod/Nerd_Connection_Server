ee5f8e1f9d7edba8542e34acf3126452d8f912e1
qlsod <rlatpals100@naver.com>
2024-08-22 17:35:31 +0900

    기본 생성자 추가 어노테이션 추가

--- src/main/java/pallet_spring/model/NicknamePatchDto.java
+++ src/main/java/pallet_spring/model/NicknamePatchDto.java
@@ -2,10 +2,12 @@ package pallet_spring.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor // 기본 생성자 추가
 @AllArgsConstructor
 public class NicknamePatchDto {
 
